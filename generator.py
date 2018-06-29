b608b54e31a090f5314e0c3285d4b62c7ae9114f
ferrisxie <ferrisxie@tencent.com>
2018-06-29 17:00:53 +0800

    add .gitignore

--- generator.py
+++ generator.py
@@ -1,15 +1,53 @@
 #encoding:utf-8
 from lib.exception import  GeneratorIllegalProjectNameException
 from lib import actions
+import ConfigParser
 import os.path
 def main():
+    config = ConfigParser.RawConfigParser()
+    config.read('templates/swift/swift.cfg')
+    template_project_name = config.get('template','name')
+    template_organization = config.get('template','organization')
+    template_organization_id = config.get('template','organization_id')
+
     project_name = raw_input("input project name:")
     if len(project_name) == 0:
         print 'project name is illegal'
         raise GeneratorIllegalProjectNameException
-    actions.copy_file('templates/swift',project_name)
-    actions.copy_file('templates/files/Podfile', project_name + '/files/')
 
+    organization = raw_input("input organization name(defualt is "+template_organization_id+"):")
+    if len(organization) == 0:
+        organization = template_organization
+    organization_identify = raw_input("input organization identify (defualt is "+template_organization_id+"),your bundle id will be like(localIndetify."+project_name+"):")
+
+    if len(organization_identify) == 0:
+        organization_identify = template_organization_id
+
+    #copy file
+    actions.copy_file('templates/swift/'+template_project_name,project_name+'/'+project_name)
+    actions.copy_file('templates/swift/'+template_project_name+'.xcodeproj/project.pbxproj',project_name+'/'+project_name+'.xcodeproj/project.pbxproj')
+    actions.copy_file('templates/files/Podfile', project_name)
+    #rename files
+    actions.rename_file(project_name+'/'+template_project_name,project_name+'/'+project_name)
+    #handle podfile
+    actions.replace_str_infile(project_name+'/'+'Podfile','template',project_name)
+    #handler projectfile
+    str_list = {}
+    str_list[template_project_name] = project_name
+    str_list[template_organization] = organization
+    str_list[template_organization_id] = organization_identify
+    actions.replace_str_infile(project_name+'/'+project_name+'.xcodeproj/project.pbxproj',str_list=str_list)
+    #install pod
+    #actions.pod_install(project_name+'/')
+
+def generate_config(dst):
+    config = ConfigParser.RawConfigParser()
+    config.add_section('template')
+    config.set('template','name','SwiftTemplate')
+    with open(dst,'wb') as config_file:
+        config.write(config_file)
+
+# generate_config('templates/swift/swift.cfg')
 
 if __name__ == "__main__":
     main()
--- lib/actions.py
+++ lib/actions.py
@@ -3,6 +3,8 @@ import os
 import shutil,errno
 from subprocess import call
 import ConfigParser
+from pathlib2 import Path
+
 def rename_file(src,dst):
     if check_exists(src):
         os.rename(src,dst)
@@ -19,15 +21,32 @@ def copy_file(src,dst):
             shutil.copytree(src,dst)
         except OSError as exc:
             if exc.errno == errno.ENOTDIR:
+                dir = os.path.dirname(dst)
+                print 'create dir' + dir
+                if not os.path.exists(dir):
+                    Path(dir).mkdir(exist_ok=True)
                 shutil.copy(src, dst)
             else: raise
+def remove_dir(dir):
+    if check_exists(dir):
+        shutil.rmtree(dir)
 
-def replace_str_infile(file_src,str_src,src_dst):
+def replace_str_infile(file_src,str_src = None,src_dst=None,str_list=None):
     if check_exists(file_src):
-        with open(file_src, "rt") as fin:
-            with open("out.txt", "wt") as fout:
+        with open(file_src, "r") as fin:
+            with open("temp", "w") as fout:
                 for line in fin:
-                    fout.write(line.replace(str_src, src_dst))
+                    if str_list is None:
+                        fout.write(line.replace(str_src, src_dst))
+                    else:
+                        #relace all string in str_list
+                        str_res = line
+                        for key,value in str_list.items():
+                            str_res = str_res.replace(key,value)
+                        fout.write(str_res)
+        os.remove(file_src)
+        rename_file('temp',file_src)
+
 
 def check_exists(path):
     if os.path.exists(path):
@@ -36,9 +55,10 @@ def check_exists(path):
         print 'no such file or directory' + path;
         return False
 
-def pod_install():
+def pod_install(dir):
     print 'start install CocoaPods ======================'
+    call(['ls'])
     try:
-        call(['pod','install','--verbose','--no-repo-update'])
+        call(['pod','install','--verbose','--no-repo-update'],cwd=dir)
     except:
         print 'fatal error: pod install failed'
